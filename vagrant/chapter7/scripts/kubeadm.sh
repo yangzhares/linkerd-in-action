dac9b26c240cb47e82db84c3d8cd7de8a0d3c3fa
Yuhongjie <snowwolfjay@foxmail.com>
2023-04-12 12:23:12 +0800

    Google not available in chine, use aliyun mirror
    and fix install 1.9.3 fail error in centos.7

--- vagrant/chapter7/scripts/kubeadm.sh
+++ vagrant/chapter7/scripts/kubeadm.sh
@@ -4,17 +4,19 @@
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+exclude=kube*
 EOF
 
 # disable selinux
 setenforce 0
 sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
 
-yum install -y kubelet-1.9.3 kubeadm-1.9.3 kubectl-1.9.3
+yum install -y  kubernetes-cni-0.6.0 kubelet-1.9.3 kubeadm-1.9.3 kubectl-1.9.3 --disableexcludes=kubernetes
+
 systemctl enable kubelet
 systemctl start kubelet
